465bf0cc4969c0a0b271b04c821ebc2ceb54a32c
Kyle Perkins <kyperkins@gmail.com>
2024-02-15 02:04:10 -0500

    finish step 25

--- project1/src/components/Main.js
+++ project1/src/components/Main.js
@@ -1,5 +1,14 @@
 export default function Main() {
     return (
-     <h1>Main goes here</h1>
+     <main className="main">
+        <h1 className="main--title">Fun facts about React</h1>
+        <ul className="main--facts">
+          <li>Was first releasted in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has over 100k stars on <a href="https://github.com/facebook/react">Github</a></li>
+          <li>Is maintained by <a href="https://facebook.com">Facebook</a></li>
+          <li>Powers thousands of enterprise apps, including Mobile apps</li>
+        </ul>
+      </main>
     );
   }
