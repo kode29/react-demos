51ce04e18eb3962881c6ff0dd1a0cf6d15793298
Kyle Perkins <kyperkins@gmail.com>
2024-02-15 01:54:37 -0500

    add style

--- project1/src/components/Navbar.js
+++ project1/src/components/Navbar.js
@@ -1,13 +1,11 @@
 export default function Navbar() {
     return (
      <nav className="navbar">
-        <div>
-          <img src="/assets/img/logo192.png" alt="React Logo"/>
-          <h3>React Facts</h3>
-        </div>
-        <div>
-          <h4>React Course - Project 1</h4>
-        </div>
+        
+          <img src="/assets/img/logo192.png" alt="React Logo" className="nav--icon"/>
+          <h3 className="nav--logo_text">React Facts</h3>
+          <h4 className="nav--title">React Course - Project 1</h4>
+        
      </nav>
     );
   }
