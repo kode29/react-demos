427294b15bdcd4c2db5a075852898fd02adec401
Kyle Perkins <kyperkins@gmail.com>
2024-02-17 17:25:34 -0500

    finish

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -13,13 +13,10 @@ export default function App() {
     return (
       <Card
         key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
+        // item={item}
+        {...item}
+        // spread can pass as "item={item}" allowing for propery passing
+        // props.item.X --> props.X
       />
     )
   })
@@ -27,7 +24,7 @@ export default function App() {
   return (
     <div className="container">
       <Navbar />
-      {/* <Hero /> */}
+      <Hero />
       <section className="cardGrid">
         {cards}
       </section>
--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -11,12 +11,12 @@ export default function Card(props){
         <div className="card">
             <div className="card--item">
                 {badgeText && <div className="card--item_badge">{badgeText}</div>}
-                <img src={`/assets/img/cards/${props.img}`} alt={props.name} className="card--item_img"/>
+                <img src={`/assets/img/cards/${props.coverImg}`} alt={props.name} className="card--item_img"/>
                 <div className="card--item_content">
                     <div className="card--item_content_rating">
                         <img src="/assets/img/cards/Star 1.png" alt="Star" className="card--item_content_star"/>
-                        <span>{props.rating}</span>
-                        <span className="gray">({props.reviewCount})</span>
+                        <span>{props.stats.rating}</span>
+                        <span className="gray">({props.stats.reviewCount})</span>
                         &middot;
                         <span className="gray">{props.location}</span>
                     </div>
