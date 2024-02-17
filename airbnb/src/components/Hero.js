eccf29e458a56e7667733dff776bbb6121969709
Kyle Perkins <kyperkins@gmail.com>
2024-02-17 00:18:01 -0500

    finish hero

--- airbnb/src/components/Hero.js
+++ airbnb/src/components/Hero.js
@@ -1,9 +1,11 @@
 export default function Hero(){
     return (
-        <div className="hero">
+        <section className="hero">
+            <img src="/assets/img/Group 77.png" alt="Photos" className="hero--img" />
             <div className="hero--content">
-                Images
+                <h1 className="hero--header">Online Experiences</h1>
+                <p className="hero--p">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
             </div>
-        </div>
+        </section>
     )
 }
