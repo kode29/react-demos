ee3f506014ef8ae1b0db520fee949c1d80a752cc
Kyle Perkins <kyperkins@gmail.com>
2024-02-18 03:01:16 -0500

    finish journal

--- personaltraveljournal/src/components/Card.js
+++ personaltraveljournal/src/components/Card.js
@@ -1,7 +1,34 @@
-export default function Card(){
+export default function Card(props){
+    console.log(props);
     return (
+
         <div className="card">
-            Card
+            <a href={props.articleLink} target="_blank">
+                <img src={props.imageUrl} alt={props.title} className="card--img" />
+            </a>
+            <div className="card--info">
+                <div className="card--info_header">
+                    <img src="/assets/img/map_pin.png" alt="Map Pin" className="card--info_map_pin"/>
+                    <span>{props.location}</span> 
+                    {props.googleMapsUrl && <a href={props.googleMapsUrl} className="card--info_map_link" target="_blank">View on Google Maps</a>}
+                </div>
+                <div className="card--info_title">{props.title}</div>
+                <div className="card--info_dates">
+                    {props.startDate} {props.endDate && ` - ${props.endDate}`} 
+                </div>
+                <div className="card--info_description">{props.description}</div>
+            </div>
         </div>
     )
 }
+
+
+{/* id: 1,
+title: "Osaka Castle",
+location: "Osaka, Japan",
+googleMapsUrl: "",
+startDate: "",
+endDate: "",
+description: "", 
+imageUrl
+*/}
--- personaltraveljournal/src/data.js
+++ personaltraveljournal/src/data.js
@@ -1,12 +1,57 @@
 export default [
     {
         id: 1,
-        title: "",
-        location: "",
-        googleMapsUrl: "",
-        startDate: "",
+        title: "Osaka Castle",
+        location: "Osaka, Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/HNDXAUcc6BCP3mPv6",
+        articleLink: "https://japanoblog.com/going-out-with-a-bang-tour-e1-osaka",
+        startDate: "10 Apr, 2018",
+        endDate: "13 Apr, 2018",
+        description: "Osaka Castle (大坂城 or 大阪城, Ōsaka-jō) is a Japanese castle in Chūō-ku, Osaka, Japan. The castle and fortress are one of Japan's most famous landmarks and it played a major role in the unification of Japan during the sixteenth century of the Azuchi-Momoyama period.",
+        imageUrl: "https://live.staticflickr.com/4774/40725146502_dfb968e88e_b.jpg"
+    },
+    {
+        id: 2,
+        title: "Hogwarts (Japan)",
+        location: "Universal Studios, Osaka, Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/GuUNyUWPccUUHQDh8",
+        articleLink: "https://japanoblog.com/going-out-with-a-bang-tour-e2-usj",
+        startDate: "12 Apr, 2018",
         endDate: "",
-        description: "",
-        imageUrl: ""
+        description: "Hogwarts Castle was a large, seven-storey high building supported by magic, with a hundred and forty-two staircases throughout its many towers and turrets and very deep dungeons. The castle was built in the late Early Middle Ages (c. 993) by a wizard architect and the four most celebrated wizards of the age: Godric Gryffindor, Helga Hufflepuff, Rowena Ravenclaw and Salazar Slytherin. The castle was the main building of Hogwarts School of Witchcraft and Wizardry, regarded as the finest wizarding school in the world",
+        imageUrl: "https://live.staticflickr.com/795/39178760470_834473bc69_b.jpg"
+    },
+    {
+        id: 3,
+        title: "Fushimi Inari Shrine",
+        location: "Kyoto, Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/8fvtRT8exxQsbLhX7",
+        articleLink: "https://japanoblog.com/going-out-with-a-bang-tour-e3-kyotonara",
+        startDate: "13 Apr, 2018",
+        endDate: "14 Apr, 2018",
+        description: "Fushimi Inari-taisha (Japanese: 伏見稲荷大社) is the head shrine of the kami Inari, located in Fushimi-ku, Kyoto, Kyoto Prefecture, Japan. The shrine sits at the base of a mountain also named Inari which is 233 metres (764 ft) above sea level, and includes trails up the mountain to many smaller shrines which span 4 kilometres (2.5 mi) and take approximately 2 hours to walk up.",
+        imageUrl: "https://live.staticflickr.com/4783/40094746625_a6b7b3fddc_b.jpg"
+    },
+    {
+        id: 4,
+        title: "Ishidatami Gate",
+        location: "Ikutsushima Shrine, Miyajima, Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/GxyrU6wrEaVXvVRb7",
+        articleLink: "https://japanoblog.com/going-out-with-a-bang-tour-e6-hiroshimamiyajima-part-2",
+        startDate: "16 Apr, 2018",
+        endDate: "17 Apr, 2018",
+        description: "Itsukushima Shrine (厳島神社 (嚴島神社), Itsukushima-jinja) is a Shinto shrine on the island of Itsukushima (popularly known as Miyajima), best known for its \"floating\" torii. It is in the city of Hatsukaichi, in Hiroshima Prefecture in Japan, accessible from the mainland by ferry at Miyajimaguchi Station. The shrine complex is listed as a UNESCO World Heritage Site, and the Japanese government has designated several buildings and possessions as National Treasures",
+        imageUrl: "https://live.staticflickr.com/4784/40988931641_66e78aaa98_b.jpg"
+    },
+    {
+        id: 5,
+        title: "Shuri Castle",
+        location: "Okinawa, Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/QAhLws5atbHXLxJC6",
+        articleLink: "https://japanoblog.com/going-out-with-a-bang-tour-e9-okinawa-3",
+        startDate: "18 Apr, 2018",
+        endDate: "",
+        description: "Shuri Castle (首里城, Shuri-jō, Okinawan: Sui Ugusuku) is a Ryukyuan gusuku castle in Shuri, Okinawa Prefecture, Japan. Between 1429 and 1879, it was the palace of the Ryukyu Kingdom, before becoming largely neglected. In 1945, during the Battle of Okinawa, it was almost completely destroyed. After the war, the castle was re-purposed as a university campus. Beginning in 1992, the central citadel and walls were largely reconstructed on the original site based on historical records, photographs, and memory. In 2000, Shuri Castle was designated as a World Heritage Site, as a part of the Gusuku Sites and Related Properties of the Kingdom of Ryukyu. On the morning of 31 October 2019, the main courtyard structures of the castle were again destroyed in a fire.",
+        imageUrl: "https://live.staticflickr.com/4795/39180087400_7e560a75d1_k.jpg"
     }
 ]
