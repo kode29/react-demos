100b2373d2c87f856800c96af0806371d43eb89e
Kyle Perkins <kyperkins@gmail.com>
2024-02-21 01:05:43 -0500

    add button onClick handler

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -1,10 +1,17 @@
+import memesData from "./memesData";
+
 export default function Meme(){
+    function handleOnClick(e){
+        e.preventDefault();
+        console.log("I was clicked");
+    }    
+    
     return (
         <main>
             <form className="form">
                 <input type="text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button">Get a new meme image &#128444;</button>
+                <button onClick={handleOnClick} className="form--button">Get a new meme image &#128444;</button>
             </form>
         </main>
     )
