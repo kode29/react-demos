8d451d2f013a5bc519b8fe09953028787b9571a6
Kyle Perkins <kyperkins@gmail.com>
2024-02-22 01:37:39 -0500

    add function to get random img

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -1,17 +1,22 @@
 import memesData from "./memesData";
 
 export default function Meme(){
-    function handleOnClick(e){
+    function getMemeImage(e){
         e.preventDefault();
-        console.log("I was clicked");
-    }    
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        console.log(`Random Number: ${randomNumber}`)
+        const url = memesArray[randomNumber].url
+        // const {url} = memesArray[randomNumber]
+        console.log(url)
+    }
     
     return (
         <main>
             <form className="form">
                 <input type="text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button onClick={handleOnClick} className="form--button">Get a new meme image &#128444;</button>
+                <button onClick={getMemeImage} className="form--button">Get a new meme image &#128444;</button>
             </form>
         </main>
     )
