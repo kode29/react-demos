bddf5809cff742782b4f72a7a71656776f660a56
Kyle Perkins <kyperkins@gmail.com>
2024-02-26 23:45:20 -0500

    add text input and state change

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -23,16 +23,39 @@ export default function Meme(){
             randomImage: url
         }))
     }
+
+    function handleChange(e){
+        const {name, value, type, checked} = e.target
+        setMeme(prevData => ({    
+            ...prevData,
+            [name]: value
+            })
+        );
+    }
     
     return (
         <main>
             <form className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Top text"
+                    onChange={handleChange} 
+                    name="topText"
+                />
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Bottom text"
+                    onChange={handleChange} 
+                    name="bottomText"
+                />
                 <button onClick={getMemeImage} className="form--button">Get a new meme image &#128444;</button>
             </form>
-            <div className="meme--image">
-                <img src={meme.randomImage} alt=""/>
+            <div className="meme">
+                <img src={meme.randomImage} alt="" className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
     )
