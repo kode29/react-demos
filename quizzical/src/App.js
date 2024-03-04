9448b006d522d620adbcaf1e5b3301a3a4269880
Kyle Perkins <kyperkins@gmail.com>
2024-03-04 00:20:31 -0500

    start blob

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -6,6 +6,8 @@ import Quiz from "./components/Quiz"
 export default function App(){
   return (
     <div className="container">
+      <div className="yellowBlob"></div>
+      <div className="blueBlob"></div>
       <Quiz />
     </div>
   )
