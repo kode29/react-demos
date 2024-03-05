51027759916f36d96b1eb53cf54468309757d692
Kyle Perkins <kyperkins@gmail.com>
2024-03-04 23:04:47 -0500

    update image assets

--- airbnb/src/components/Hero.js
+++ airbnb/src/components/Hero.js
@@ -1,7 +1,7 @@
 export default function Hero(){
     return (
         <section className="hero">
-            <img src="%PUBLIC_URL%/assets/img/Group 77.png" alt="Photos" className="hero--img" />
+            <img src="./assets/img/Group 77.png" alt="Photos" className="hero--img" />
             <div className="hero--content">
                 <h1 className="hero--header">Online Experiences</h1>
                 <p className="hero--p">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
